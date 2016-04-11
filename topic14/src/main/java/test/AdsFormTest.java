7207b698d0bd901b74ccf3014c955b48ef6af685
cyxouD <cyxoud@mail.ru>
2016-04-11 12:35:48 +0300

    Refactored test code

--- topic14/src/main/java/test/AdsFormTest.java
+++ topic14/src/main/java/test/AdsFormTest.java
@@ -7,6 +7,7 @@ import db.CarAdDao;
 import db.CarSearchParameter;
 import db.UsedCarsTradeDB;
 import org.json.simple.JSONObject;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -42,7 +43,12 @@ public class AdsFormTest {
 
     @BeforeClass
     public static void initialize(){
-        adsForm = new AdsForm();
+        UsedCarsTradeDB.init();
+    }
+
+    @AfterClass
+    public static void destroy(){
+        UsedCarsTradeDB.closeConnection();
     }
 
     @Before
@@ -51,7 +57,7 @@ public class AdsFormTest {
     }
 
     @Test
-    public void testFormCarParameters(){
+    public void testFormCarParameters() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException{
         CarSearchParameter[] carSearchParameters = new CarSearchParameter[ServletConstants.NUMBER_OF_SEARCH_CRITERIA];
         carSearchParameters[0] = new CarSearchParameter("Mazda");
         carSearchParameters[1] = new CarSearchParameter("ML");
@@ -68,20 +74,11 @@ public class AdsFormTest {
         when(httpServRequestMock.getParameter("priceTo")).thenReturn(carSearchParameters[5].toString());
 
         CarSearchParameter[] carSearchParametersFromMethod = null;
-        try {
-            Method methodFormCarParameters =  AdsForm.class.getDeclaredMethod(METHOD_NAME.formCarParameters.toString(),
+        Method methodFormCarParameters =  AdsForm.class.getDeclaredMethod(METHOD_NAME.formCarParameters.toString(),
                     HttpServletRequest.class);
-            methodFormCarParameters.setAccessible(true);
-            carSearchParametersFromMethod = (CarSearchParameter[]) methodFormCarParameters.invoke(AdsForm.class,
+        methodFormCarParameters.setAccessible(true);
+        carSearchParametersFromMethod = (CarSearchParameter[]) methodFormCarParameters.invoke(AdsForm.class,
                     httpServRequestMock);
-        } catch (NoSuchMethodException e) {
-            System.out.println("Method" + METHOD_NAME.formCarParameters.toString() + " wasn't found");
-        } catch (IllegalAccessException e) {
-            System.out.println("Method" + METHOD_NAME.formCarParameters.toString() + " is private");
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            System.err.format(METHOD_NAME.formCarParameters.toString() + " failed: %s%n", cause.getMessage());
-        }
 
         assertArrayEquals(carSearchParameters, carSearchParametersFromMethod);
         assertThat(not(carSearchParameters), is(not(carSearchParametersFromMethod)));
@@ -89,7 +86,7 @@ public class AdsFormTest {
     }
 
     @Test
-    public void testGetPostText(){
+    public void testGetPostText() throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException{
         String input = "{\n" +
                 "\"manufacturer\": \"Mazda\",\n" +
                 "\"modelName\": \"Mazda3\",\n" +
@@ -99,43 +96,29 @@ public class AdsFormTest {
                 "\"price\": 9999,\n" +
                 "\"contact_phone\": \"+380-66-22-333\"\n" +
                 "}";
-            BufferedReader bufferedReaderMock = Mockito.mock(BufferedReader.class);
-        try {
-            when(bufferedReaderMock.readLine()).thenReturn("{\n").thenReturn("\"manufacturer\": \"Mazda\",\n").
+        BufferedReader bufferedReaderMock = Mockito.mock(BufferedReader.class);
+
+        when(bufferedReaderMock.readLine()).thenReturn("{\n").thenReturn("\"manufacturer\": \"Mazda\",\n").
                     thenReturn("\"modelName\": \"Mazda3\",\n").thenReturn("\"year\": 2011,\n").
                     thenReturn("\"vin\": \"ABC123456\",\n").thenReturn("\"description\": \"Test\",\n").
                     thenReturn("\"price\": 9999,\n").thenReturn("\"contact_phone\": \"+380-66-22-333\"\n").
                     thenReturn("}").thenReturn(null);
-            when(httpServRequestMock.getReader()).thenReturn(bufferedReaderMock);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        when(httpServRequestMock.getReader()).thenReturn(bufferedReaderMock);
 
         String resultString = null;
-        try {
-            Method methodGetPostText =  AdsForm.class.getDeclaredMethod(METHOD_NAME.getPostText.toString(),
+
+        Method methodGetPostText =  AdsForm.class.getDeclaredMethod(METHOD_NAME.getPostText.toString(),
                     HttpServletRequest.class);
-            methodGetPostText.setAccessible(true);
-            resultString = (String) methodGetPostText.invoke(AdsForm.class, httpServRequestMock);
-        } catch (NoSuchMethodException e) {
-            System.out.println("Method" + METHOD_NAME.getPostText.toString() + " wasn't found");
-        } catch (IllegalAccessException e) {
-            System.out.println("Method" + METHOD_NAME.getPostText.toString() + " is private");
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            System.err.format(METHOD_NAME.getPostText.toString() + " failed: %s%n", cause.getMessage());
-        }
-        try {
-            verify(bufferedReaderMock,times(10)).readLine();
-            assertEquals(input,resultString);
-            assertThat(not(input), is(not(resultString)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        methodGetPostText.setAccessible(true);
+        resultString = (String) methodGetPostText.invoke(AdsForm.class, httpServRequestMock);
+
+        verify(bufferedReaderMock,times(10)).readLine();
+        assertEquals(input,resultString);
+        assertThat(not(input), is(not(resultString)));
     }
 
     @Test
-    public void testProduceCarObjectFromParameters(){
+    public void testProduceCarObjectFromParameters() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException{
         Car car = new Car("5654654645HL", "Mersedes", "XL", 1500, "good car", 50000);
 
         Map<String,Object> jsonMap = new HashMap<>();
@@ -150,106 +133,60 @@ public class AdsFormTest {
 
 
         Car resultCar = null;
-        try {
-            Method methodProduceCarObjectFromParameters =  AdsForm.class.getDeclaredMethod
+        Method methodProduceCarObjectFromParameters =  AdsForm.class.getDeclaredMethod
                     (METHOD_NAME.produceCarObjectFromParameters.toString(),
                     Object.class);
-            methodProduceCarObjectFromParameters.setAccessible(true);
-            resultCar = (Car) methodProduceCarObjectFromParameters.invoke(AdsForm.class, carObject);
-        } catch (NoSuchMethodException e) {
-            System.out.println("Method" + METHOD_NAME.produceCarObjectFromParameters.toString() + " wasn't found");
-        } catch (IllegalAccessException e) {
-            System.out.println("Method" + METHOD_NAME.produceCarObjectFromParameters.toString() + " is private");
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            System.err.format(METHOD_NAME.produceCarObjectFromParameters.toString() + " failed: %s%n", cause.getMessage());
-        }
+        methodProduceCarObjectFromParameters.setAccessible(true);
+        resultCar = (Car) methodProduceCarObjectFromParameters.invoke(AdsForm.class, carObject);
 
         assertEquals(car, resultCar);
         assertThat(not(car),is(not(resultCar)));
     }
 
     @Test
-    public void testFormJsonStringToPrint(){
-        UsedCarsTradeDB.init();
+    public void testFormJsonStringToPrint() throws SQLException, NoSuchMethodException, IllegalAccessException, InvocationTargetException{
         List<String> carAdsList;
-        try {
-            carAdsList = CarAdDao.getAllAds();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        carAdsList = CarAdDao.getAllAds();
 
         //must be added
 
         carAdsList = new ArrayList<>();
         String resultMethodString = null;
-        try {
-            Method methodFormJsonStringToPrint =  AdsForm.class.getDeclaredMethod
+        Method methodFormJsonStringToPrint =  AdsForm.class.getDeclaredMethod
                     (METHOD_NAME.formJsonStringToPrint.toString(), List.class);
-            methodFormJsonStringToPrint.setAccessible(true);
-            resultMethodString = (String) methodFormJsonStringToPrint.invoke(AdsForm.class, carAdsList);
-        } catch (NoSuchMethodException e) {
-            System.out.println("Method" + METHOD_NAME.formJsonStringToPrint.toString() + " wasn't found");
-        } catch (IllegalAccessException e) {
-            System.out.println("Method" + METHOD_NAME.formJsonStringToPrint.toString() + " is private");
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            System.err.format(METHOD_NAME.formJsonStringToPrint.toString() + " failed: %s%n", cause.getMessage());
-        }
+        methodFormJsonStringToPrint.setAccessible(true);
+        resultMethodString = (String) methodFormJsonStringToPrint.invoke(AdsForm.class, carAdsList);
 
         String noResultString = "<h1>" + "No results" + "</h1>";
         assertEquals(noResultString, resultMethodString);
     }
 
     @Test
-    public void testDoGet(){
-        UsedCarsTradeDB.init();
-
+    public void testDoGet() throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException{
         HttpServletResponse httpServletResponseMock = Mockito.mock(HttpServletResponse.class);
 
         Enumeration enumeration = Mockito.mock(Enumeration.class);
         when (enumeration.hasMoreElements()).thenReturn(true);
         when(httpServRequestMock.getParameterNames()).thenReturn(enumeration);
 
-        try {
-            PrintWriter printWriter = Mockito.mock(PrintWriter.class);
-            when(httpServletResponseMock.getWriter()).thenReturn(printWriter);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        PrintWriter printWriter = Mockito.mock(PrintWriter.class);
+        when(httpServletResponseMock.getWriter()).thenReturn(printWriter);
 
         BufferedReader bufferedReaderMock = Mockito.mock(BufferedReader.class);
-        try {
-            when(bufferedReaderMock.readLine()).thenReturn("{\n").thenReturn("\"manufacturer\": \"Mazda\",\n").
+        when(bufferedReaderMock.readLine()).thenReturn("{\n").thenReturn("\"manufacturer\": \"Mazda\",\n").
                     thenReturn("\"modelName\": \"Mazda3\",\n").thenReturn("\"year\": 2011,\n").
                     thenReturn("\"vin\": \"ABC123456\",\n").thenReturn("\"description\": \"Test\",\n").
                     thenReturn("\"price\": 9999,\n").thenReturn("\"contact_phone\": \"+380-66-22-333\"\n").
                     thenReturn("}").thenReturn(null);
-            when(httpServRequestMock.getReader()).thenReturn(bufferedReaderMock);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        when(httpServRequestMock.getReader()).thenReturn(bufferedReaderMock);
 
 
-        try {
-            Method methodDoGet =  AdsForm.class.getDeclaredMethod
+        Method methodDoGet =  AdsForm.class.getDeclaredMethod
                     (METHOD_NAME.doGet.toString(), HttpServletRequest.class, HttpServletResponse.class);
-            methodDoGet.setAccessible(true);
-            methodDoGet.invoke(new AdsForm(), httpServRequestMock, httpServletResponseMock);
-        } catch (NoSuchMethodException e) {
-            System.out.println("Method" + METHOD_NAME.doGet.toString() + " wasn't found");
-        } catch (IllegalAccessException e) {
-            System.out.println("Method" + METHOD_NAME.doGet.toString() + " is private");
-        } catch (InvocationTargetException e) {
-            Throwable cause = e.getCause();
-            System.err.format(METHOD_NAME.doGet.toString() + " failed: %s%n", cause.getMessage());
-        }
+        methodDoGet.setAccessible(true);
+        methodDoGet.invoke(new AdsForm(), httpServRequestMock, httpServletResponseMock);
 
-        try {
-            verify(httpServletResponseMock,times(1)).getWriter();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        verify(httpServletResponseMock,times(1)).getWriter();
     }
     
 }
